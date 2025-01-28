8dcd1a46c6a8fdc594f48cc8b1e2021bef353836
mehdi <dridignu@gmail.com>
2025-01-28 22:21:50 +0100

    add unit tests

--- src/sync.py
+++ src/sync.py
@@ -22,7 +22,7 @@ def sync(source: Path, destination: Path):
     source_hashes = {}
     for folder, _, files in os.walk(source):
         for file_name in files:
-            source_path = Path(folder)/file_name
+            source_path = Path(folder) / file_name
             source_hash = hash_file(source_path)
             source_hashes[source_hash] = file_name
 
@@ -30,19 +30,24 @@ def sync(source: Path, destination: Path):
 
     for folder, _, files in os.walk(destination):
         for file_name in files:
-            destination_path = Path(folder)/file_name
+            destination_path = Path(folder) / file_name
             destination_hash = hash_file(destination_path)
             seen.add(destination_hash)
 
             if destination_hash not in source_hashes:
                 destination_path.unlink()
-            elif destination_hash in source_hashes and source_hashes[destination_hash] != file_name:
-                shutil.move(destination_path, Path(folder)/source_hashes[destination_hash])
+            elif (
+                destination_hash in source_hashes
+                and source_hashes[destination_hash] != file_name
+            ):
+                shutil.move(
+                    destination_path, Path(folder) / source_hashes[destination_hash]
+                )
 
     for source_hash, file_name in source_hashes.items():
         if source_hash not in seen:
             shutil.copy(Path(source) / file_name, Path(destination) / file_name)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sync(Path(sys.argv[1]), Path(sys.argv[2]))
---
+++ tests/test_sync.py
@@ -0,0 +1,47 @@
+import shutil
+import tempfile
+import unittest
+from pathlib import Path
+
+from src.sync import sync
+
+
+class TestSync(unittest.TestCase):
+    def test_when_a_file_exists_in_the_source_but_not_the_destination(self):
+        try:
+            source = tempfile.mkdtemp()
+            dest = tempfile.mkdtemp()
+
+            content = "I am a very useful file"
+            (Path(source) / "my-file").write_text(content)
+
+            sync(Path(source), Path(dest))
+
+            expected_path = Path(dest) / "my-file"
+            assert expected_path.exists()
+            assert expected_path.read_text() == content
+
+        finally:
+            shutil.rmtree(source)
+            shutil.rmtree(dest)
+
+    def test_when_a_file_has_been_renamed_in_the_source(self):
+        try:
+            source = tempfile.mkdtemp()
+            dest = tempfile.mkdtemp()
+
+            content = "I am a file that was renamed"
+            source_path = Path(source) / "source-filename"
+            old_dest_path = Path(dest) / "dest-filename"
+            expected_dest_path = Path(dest) / "source-filename"
+            source_path.write_text(content)
+            old_dest_path.write_text(content)
+
+            sync(Path(source), Path(dest))
+
+            assert old_dest_path.exists() is False
+            assert expected_dest_path.read_text() == content
+
+        finally:
+            shutil.rmtree(source)
+            shutil.rmtree(dest)
